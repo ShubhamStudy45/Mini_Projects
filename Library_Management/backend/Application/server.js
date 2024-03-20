56b45496b7add7e614d6b1e86e81477f9aaf5992
Mini_Projects <sableshubham890@gmail.com>
2024-03-20 22:18:48 +0530

    server file

---
+++ Library_Management/backend/Application/server.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser')
+
+//application routes
+const userRouter = require('./routes/user')
+const bookRouter = require('./routes/book')
+
+const config = require('./config')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.use((request,response, next)=>{
+
+    const token = request.headers['token']
+
+    if(request.url.startsWith('/user/signin') ||
+    request.url.startsWith('/user/signup')){
+        next()
+    }else{
+        try{
+            // console.log(token)
+            const payload = jwt.verify(token,config.secrete)
+            request.userId = payload['id']
+            // console.log(request.userId)
+            
+            next() 
+        }catch(ex){
+           response.send({
+            status : 'error',
+            error : "Unauthrized access..!!"
+           })
+        }
+    
+    }
+
+})
+app.use('/user/',userRouter)
+app.use('/book',bookRouter)
+app.listen(4000,'0.0.0.0',()=>{
+    console.log('server started on port 4000')
+})
