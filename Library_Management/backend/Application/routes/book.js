25b20468f4e7549ac7390db260cea844e496e711
Mini_Projects <sableshubham890@gmail.com>
2024-03-25 22:54:24 +0530

    add book API

--- Library_Management/backend/Application/routes/book.js
+++ Library_Management/backend/Application/routes/book.js
@@ -9,11 +9,22 @@ const route = express.Router()
 route.get('/find-book',(request, response)=>{
 
     const { name } = request.body
-    const statement = `select * from book where name like '${name}'`
+    const statement = `select * from book where name like '%${name}%'`
     // console.log(statement)
     db.execute(statement,(error,data)=>{
         response.send(utils.createResult(error,data))
     })
 })
+// add book
+route.post('/add-book',(request, response)=>{
+    
+    const { name, author, subject, price } = request.body
 
+    const statement = `insert into book (name, author, subject, price, status )
+                        values
+                        ('${name}','${author}','${subject}','${price}', 0 )`
+    db.execute(statement,(error, data)=>{
+        response.send(utils.createResult(error,data))
+    })
+})
 module.exports = route
